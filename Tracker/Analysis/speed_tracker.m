6ad71236222d27b32e991c2bc1a72ebd3da8cd0f
Jens Thielemann <jens.thielemann@gmail.com>
2023-07-15 14:10:37 +0200

    Update speed_tracker.m

--- Tracker/Analysis/speed_tracker.m
+++ Tracker/Analysis/speed_tracker.m
@@ -12,35 +12,48 @@ L = 1000;
 % Create a time base
 t = (0:L-1)*T;
 
-v_o = zeros(1,L);
+v_o_in = zeros(1,L);
+v_o_out = zeros(1,L);
+v_o_mean = zeros(1,L);
 v_t = zeros(1,L);
-v_c = zeros(1,L);
 
-p_t = zeros(1,L); % -> p_c
-
-p_o = sin((6.28*Fs/500*t)+1.25);
+%p_t = zeros(1,L); % -> p_c
+p_t = 0;
+p_o_in = (10*sin((6.28*Fs/500*t)+1.25))+10;
+p_o_out = zeros(1,L);
 
 T_c = 5;
 
-for i=1:L
-  v_c(i) = (p_o(i) - p_t(i))/T_c;
+p_o_out(1) = p_o_in(1);
+
+for i=1:L-1
   if i > 1
-    v_o(i) = ((p_o(i) - p_t(i)) - (p_o(i-1) - p_t(i-1)))/T;
+    % System behavior
+    v_o_in(i) = (p_o_in(i) - p_o_in(i-1))/T;
+    p_o_out(i) = ((v_o_in(i) - v_t(i)) * T) + p_o_out(i-1) - p_t;
+    % System behavior end
+
+    %v_t(i+1) = (p_o_out(i) - p_t)/T_c;
+    v_o_out(i) = (p_o_out(i) - p_o_out(i-1) + p_t + (v_t(i)*T))/T;
+    v_t(i+1) = v_o_out(i)+((p_o_out(i) - p_t)/T_c);
   endif
 end
 
 figure(1)
 clf
-subplot(1,1,1)
-plot(t, v_c, 'b')
+subplot(2,1,1)
+plot(t, p_o_in, 'b')
 hold
-plot(t, v_o, 'g')
+plot(t, p_o_out, 'g')
 hold
+subplot(2,1,2)
+plot(t, v_o_in, 'b')
+hold
+plot(t, v_o_out, 'g')
+hold
+
 %xlim([0 640])
 %ylim([-2 2])
 
-figure(2)
-clf
-subplot(1,1,1)
-plot(t, p_o)
-hold
+
+
